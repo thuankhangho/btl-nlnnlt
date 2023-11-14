04a3d7f5e3d96dce5420336435c01df564de421e
thuankhangho <hothuankhang@gmail.com>
2023-11-14 23:20:15 +0700

    Added some testcases for Assignment 3

--- assignment3/src/test/CheckSuite.py
+++ assignment3/src/test/CheckSuite.py
@@ -3,43 +3,43 @@ from TestUtils import TestChecker
 from AST import *
  
 class CheckSuite(unittest.TestCase):
-    # def test_400(self):
-    #     input = Program([ClassDecl(Id("a"),[])])
-    #     expect = "No Entry Point"
-    #     self.assertTrue(TestChecker.test(input,expect,400))
+    def test_400(self):
+        input = Program([ClassDecl(Id("a"),[])])
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input,expect,400))
         
-    # def test_401(self):
-    #     input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("main"),[],VoidType(),Block([])),AttributeDecl(VarDecl(Id("c"),IntType())),AttributeDecl(VarDecl(Id("c"),IntType()))])])
-    #     expect = "Redeclared Attribute: c"
-    #     self.assertTrue(TestChecker.test(input,expect,401))
+    def test_401(self):
+        input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("main"),[],VoidType(),Block([])),AttributeDecl(VarDecl(Id("c"),IntType())),AttributeDecl(VarDecl(Id("c"),IntType()))])])
+        expect = "Redeclared Attribute: c"
+        self.assertTrue(TestChecker.test(input,expect,401))
         
-    # def test_402(self):
-    #     input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("main"),[],VoidType(),Block([])),AttributeDecl(VarDecl(Id("c"),IntType())),MethodDecl(Id("c"),[],IntType(),Block([]))])])
-    #     expect = "Redeclared Method: c"
-    #     self.assertTrue(TestChecker.test(input,expect,402))
+    def test_402(self):
+        input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("main"),[],VoidType(),Block([])),AttributeDecl(VarDecl(Id("c"),IntType())),MethodDecl(Id("c"),[],IntType(),Block([]))])])
+        expect = "Redeclared Method: c"
+        self.assertTrue(TestChecker.test(input,expect,402))
         
-    # def test_403(self):
-    #     input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("main"),[],VoidType(),Block([]))]),ClassDecl(Id("c"),[]),ClassDecl(Id("c"),[])])
-    #     expect = "Redeclared Class: c"
-    #     self.assertTrue(TestChecker.test(input,expect,403))
+    def test_403(self):
+        input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("main"),[],VoidType(),Block([]))]),ClassDecl(Id("c"),[]),ClassDecl(Id("c"),[])])
+        expect = "Redeclared Class: c"
+        self.assertTrue(TestChecker.test(input,expect,403))
 
-    # def test_404(self):
-    #     input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("main"),[],VoidType(),Block([]))])])
-    #     expect = "successful"
-    #     self.assertTrue(TestChecker.test(input,expect,404))
+    def test_404(self):
+        input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("main"),[],VoidType(),Block([]))])])
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input,expect,404))
     
-    # def test_405(self):
-    #     input = Program([ClassDecl(Id("io"),[])])
-    #     expect = "Redeclared Class: io"
-    #     self.assertTrue(TestChecker.test(input,expect,405))
+    def test_405(self):
+        input = Program([ClassDecl(Id("io"),[])])
+        expect = "Redeclared Class: io"
+        self.assertTrue(TestChecker.test(input,expect,405))
     
-    # def test_406(self):
-    #     input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("@main"),[],VoidType(),Block([])),
-    #                                               ]),
-    #                      ClassDecl(Id("Test"),[MethodDecl(Id("test"),[],VoidType(),Block([])),
-    #                                               ])])
-    #     expect = ""
-    #     self.assertTrue(TestChecker.test(input,expect,406))
+    def test_406(self):
+        input = Program([ClassDecl(Id("Program"),[MethodDecl(Id("@main"),[],VoidType(),Block([])),
+                                                  ]),
+                         ClassDecl(Id("Test"),[MethodDecl(Id("test"),[],VoidType(),Block([])),
+                                                  ])])
+        expect = "[]"
+        self.assertTrue(TestChecker.test(input,expect,406))
     
     def test_407(self):
         input = """
@@ -49,6 +49,58 @@ class CheckSuite(unittest.TestCase):
             }
         }
         """
-        expect = ""
+        expect = "[]"
         self.assertTrue(TestChecker.test(input,expect,407))
         
+    def test_408(self):
+        input = """
+        class Program {
+            func @main(): void {
+                
+            }
+        }
+        class a {
+            var a: int;
+        }
+        class a <- b {
+            var a: string;
+        }
+        """
+        expect = "[]"
+        self.assertTrue(TestChecker.test(input,expect,408))
+
+    def test_408(self):
+        input = """
+        class Program {
+            func @main(): void {
+                
+            }
+        }
+        class a {
+            var a: int;
+        }
+        class b <- a {
+            var a: string;
+        }
+        """
+        expect = "Redeclared Class: a"
+        self.assertTrue(TestChecker.test(input,expect,408))
+
+    def test_409(self):
+        input = """
+        class Program {
+            func @main(): void {
+                
+            }
+        }
+        class a {
+            var a: int;
+            func constructor(a: int) {}
+            func constructor(a: string) {}
+        }
+        class b <- a {
+            var a: string;
+        }
+        """
+        expect = "Redeclared Class: a"
+        self.assertTrue(TestChecker.test(input,expect,409))
